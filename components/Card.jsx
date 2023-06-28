f8fd0d23568d7e2f7522a806b29269a8b13b1a23
yonatan-nyo <yonatan.njoto@gmail.com>
2023-06-28 20:15:32 +0700

    fixed card responsiveness

--- components/Card.jsx
+++ components/Card.jsx
@@ -13,13 +13,12 @@ const defaultBusiness = {
 
 const Card = ({ business = defaultBusiness }) => {
   return (
-    <div className="w-[96%] h-28 sm:w-[47%] md:w-[32%] sm:h-32 md:h-48 lg:h-60 bg-slate-100 mx-auto relative">
+    <div className="w-[96%] md:w-[47%] lg:w-[32%] h-48 md:h-[15rem] bg-slate-100 mx-auto relative">
       <div
         className="w-full absolute h-12 bg-blue-300"
         style={{
           background: "rgb(2,0,36)",
-          background:
-            "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%) ",
+          background: "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%) ",
         }}
       />
       <div className="flex absolute p-8 gap-8 flex-row text-right w-full h-full">
@@ -33,17 +32,13 @@ const Card = ({ business = defaultBusiness }) => {
               className="bg-white rounded-lg border-2 border-slate-200 w-full h-28 object-contain"
             />
           </div>
-          <p className="p-2 text-[20px] leading-relaxed font-semibold">
-            Category
-          </p>
+          <p className="p-2 text-lg leading-relaxed font-semibold">Category</p>
         </div>
-        <div className="flex-col py-6 w-full h-full">
-          <p className="text-left font-bold text-[27px]">{business.name}</p>
+        <div className="flex flex-col pt-6 w-full">
+          <p className="text-left font-bold text-2xl">{business.name}</p>
           <p className="text-left">Rp {Math.ceil(+business.fundNeeded / 40)}</p>
-          <div className="flex-grow h-full flex justify-end flex-col items-end pb-10">
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full h-[37px]">
-              <p>Button</p>
-            </button>
+          <div className="flex-grow flex justify-end items-end">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded-full">Invest</button>
           </div>
         </div>
       </div>
@@ -52,3 +47,13 @@ const Card = ({ business = defaultBusiness }) => {
 };
 
 export default Card;
+
+/*
+<Image
+              src={business.brandUrl}
+              alt={business?.name}
+              width={400}
+              height={400}
+              className="bg-white rounded-lg border-2 border-slate-200 w-full h-28 object-contain"
+            />
+             */
