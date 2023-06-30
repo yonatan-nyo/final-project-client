5e14b78e949ccf03ebf3de4a2744d2c125f9d830
yonatan-nyo <yonatan.njoto@gmail.com>
2023-06-30 12:12:39 +0700

    added image input

--- app/invest/add/page.js
+++ app/invest/add/page.js
@@ -4,47 +4,47 @@ import Image from "next/image";
 import "./investAdd.css";
 import { BsCardImage } from "react-icons/bs";
 import { useState } from "react";
+import ImageInput from "@/components/ImageInput";
 
 const Page = () => {
-  const [url, setUrl] = useState("");
-  const [selectedImage, setSelectedImage] = useState(null);
-
-  const changeImage = () => {
-    const input = document.createElement("input");
-    input.type = "file";
-    input.accept = "image/*";
-    input.addEventListener("change", (event) => {
-      const file = event.target.files[0];
-      if (file) {
-        const reader = new FileReader();
-        reader.onload = () => {
-          setUrl(reader.result);
-          setSelectedImage(file);
-        };
-        reader.readAsDataURL(file);
-      }
-    });
-    input.click();
-  };
-
   return (
-    <div className="max-w-[1450px] mx-auto pt-24 px-3">
-      <form className="flex gap-4">
-        <div className="w-60 h-60">
-          {url ? (
-            <Image src={url} className="w-60 h-60 object-cover" alt="logo" width={400} height={400} onClick={changeImage} />
-          ) : (
-            <div className="w-60 h-60 bg-slate-200 cursor-pointer flex justify-center items-center" onClick={changeImage}>
-              <BsCardImage size={50} className="text-slate-500" />
+    <div className="max-w-[1450px] mx-auto px-3 flex flex-col">
+      <form className="flex flex-col gap-4 w-full h-full min-h-screen pt-24 pb-4">
+        <h1 className="font-bold text-4xl text-center py-3">Add Business</h1>
+        <div className="flex gap-4 w-full h-full">
+          <div className="border-r-2 pr-4 border-slate-500 h-fit">
+            <ImageInput msg="Click to change your logo!" />
+            <div className="group-input w-full max-w-sm">
+              <label htmlFor="name">Name</label>
+              <input type="text" name="name" id="name" autoComplete="off" placeholder="Your Business Name" />
             </div>
-          )}
-        </div>
-        <div className="group-input w-full max-w-sm">
-          <label htmlFor="name">Business Name</label>
-          <input type="text" name="name" id="name" autoComplete={false} />
-        </div>
 
-        <div className="group-input"></div>
+            <div className="group-input w-full max-w-sm">
+              <label htmlFor="fundNeeded">Fund Needed</label>
+              <input type="number" name="fundNeeded" id="fundNeeded" autoComplete="off" />
+            </div>
+          </div>
+          <div className="flex-grow flex flex-col justify-between h-auto">
+            <div>
+              <label htmlFor="overview">Overview</label>
+              <textarea name="overview" id="overview" className="w-full"></textarea>
+              <label htmlFor="images">Images</label>
+              <div className="flex gap-2">
+                <ImageInput size="small" />
+                <ImageInput size="small" />
+              </div>
+            </div>
+
+            <div className="flex gap-2 justify-end">
+              <button className="flex h-12 w-fit items-center px-4 py-1 text-red-600 font-bold hover:bg-red-100 border-2 border-red-600 mb-2 rounded-lg">
+                <p className="font-medium">Cancel</p>
+              </button>
+              <button className="flex h-12 w-fit items-center px-4 py-1 text-white bg-blue-500 hover:bg-blue-800 mb-2 rounded-lg">
+                <p className="font-medium">Submit</p>
+              </button>
+            </div>
+          </div>
+        </div>
       </form>
     </div>
   );
---
+++ components/ImageInput.jsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+import { BsCardImage } from "react-icons/bs";
+
+const ImageInput = ({ imageFile, setImageFIle, size, msg = "" }) => {
+  const [url, setUrl] = useState(null);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const changeImage = () => {
+    const input = document.createElement("input");
+    input.type = "file";
+    input.accept = "image/*";
+    input.addEventListener("change", (event) => {
+      const file = event.target.files[0];
+      if (file) {
+        const reader = new FileReader();
+        reader.onload = () => {
+          setUrl(reader.result);
+          setSelectedImage(file);
+        };
+        reader.readAsDataURL(file);
+      }
+    });
+    input.click();
+  };
+
+  return (
+    <>
+      {url ? (
+        <div>
+          <Image
+            src={url}
+            className={size === "small" ? "w-32 h-32 sm:w-40 sm:h-40 object-cover" : "w-40 h-40 sm:w-60 sm:h-60 object-cover"}
+            alt="logo"
+            width={400}
+            height={400}
+            onClick={changeImage}
+          />
+          <p className="text-center font-bold">{selectedImage?.name}</p>
+          <p className="text-center font-light">*{msg}*</p>
+        </div>
+      ) : (
+        <div
+          className={
+            size === "small"
+              ? "w-32 h-32 sm:w-40 sm:h-40 bg-slate-200 cursor-pointer flex justify-center items-center flex-col"
+              : "w-40 h-40 sm:w-60 sm:h-60 bg-slate-200 cursor-pointer flex justify-center items-center flex-col"
+          }
+          onClick={changeImage}>
+          <BsCardImage size={50} className="text-slate-500" />
+          <p className="text-center font-light">{msg}</p>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default ImageInput;
