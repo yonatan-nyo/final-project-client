1737b6c4cc9e1aea8ba19726f7810b2308cd216f
yonatan-nyo <yonatan.njoto@gmail.com>
2023-06-30 08:56:14 +0700

    added social media login

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,38 +1,50 @@
+"use client";
+import SignIn from "@/components/SignIn";
 import Image from "next/image";
+import { useEffect } from "react";
 
 const Profile = () => {
+  const [signIn, setSignIn] = useState(false);
+  useEffect(() => {
+    if (!localStorage.getItem("access_token")) {
+      return setSignIn(true);
+    }
+  }, []);
   return (
-    <div className="flex flex-col pt-28 max-w-[1480px] h-screen mx-auto">
-      <div className="flex flex-wrap justify-center sm:justify-start px-6">
-        <div className="h-48 w-48 sm:w-auto rounded-lg overflow-hidden mb-4">
-          <Image
-            className="object-cover w-full h-full"
-            width={400}
-            height={400}
-            src="https://upload.wikimedia.org/wikipedia/id/thumb/d/d3/Starbucks_Corporation_Logo_2011.svg/1200px-Starbucks_Corporation_Logo_2011.svg.png"
-            alt="IMAGE"
-          />
-        </div>
-        <div className="h-auto w-auto flex flex-col items-center sm:items-start px-8 justify-between py-3">
-          <div>
-            <h2 className="text-2xl font-bold">Account Name</h2>
-            <p className="text-gray-600">john.doe@example.com</p>
+    <>
+      <SignIn setSignIn={setSignIn} signIn={signIn} />
+      <div className="flex flex-col pt-28 max-w-[1480px] h-screen mx-auto">
+        <div className="flex flex-wrap justify-center sm:justify-start px-6">
+          <div className="h-48 w-48 sm:w-auto rounded-lg overflow-hidden mb-4">
+            <Image
+              className="object-cover w-full h-full"
+              width={400}
+              height={400}
+              src="https://upload.wikimedia.org/wikipedia/id/thumb/d/d3/Starbucks_Corporation_Logo_2011.svg/1200px-Starbucks_Corporation_Logo_2011.svg.png"
+              alt="IMAGE"
+            />
           </div>
-          <button className="bg-red-500 text-white px-4 py-2 rounded-full mt-2">
-            <p className=" font-bold">LOGOUT</p>
-          </button>
+          <div className="h-auto w-auto flex flex-col items-center sm:items-start px-8 justify-between py-3">
+            <div>
+              <h2 className="text-2xl font-bold">Account Name</h2>
+              <p className="text-gray-600">john.doe@example.com</p>
+            </div>
+            <button className="bg-red-500 text-white px-4 py-2 rounded-full mt-2">
+              <p className=" font-bold">LOGOUT</p>
+            </button>
+          </div>
         </div>
-      </div>
 
-      <div className="flex flex-wrap justify-start space-y-4 px-6 mt-4 w-full">
-        <div className="flex-auto w-full flex-wrap sm:w-1/2 h-40 bg-gray-200 rounded mb-2 sm:mr-2 sm:mb-0 mr-2">
-          <p className="font-bold text-xl ml-2">Investments</p>
-        </div>
-        <div className=" flex-auto flex-wrap w-full sm:w-1/2 h-40 bg-gray-200 rounded mb-2 sm:mr-2 sm:mb-0 mr-2">
-          <p className="font-bold text-xl ml-2">Business</p>
+        <div className="flex flex-wrap justify-start space-y-4 px-6 mt-4 w-full">
+          <div className="flex-auto w-full flex-wrap sm:w-1/2 h-40 bg-gray-200 rounded mb-2 sm:mr-2 sm:mb-0 mr-2">
+            <p className="font-bold text-xl ml-2">Investments</p>
+          </div>
+          <div className=" flex-auto flex-wrap w-full sm:w-1/2 h-40 bg-gray-200 rounded mb-2 sm:mr-2 sm:mb-0 mr-2">
+            <p className="font-bold text-xl ml-2">Business</p>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- components/SignIn.jsx
+++ components/SignIn.jsx
@@ -1,27 +1,48 @@
 "use client";
 
 import React from "react";
-import { signInWithPopup } from "firebase/auth";
+import { FacebookAuthProvider, GoogleAuthProvider, TwitterAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 import { FaFacebook, FaTwitter, FaGoogle } from "react-icons/fa";
-// import firebaseInit from "@/config/firebaseConfig";
+import firebaseInit from "@/config/firebaseConfig";
 
-// firebaseInit;
+firebaseInit;
 
-// const providerGithub = new GithubAuthProvider();
-// const auth = getAuth();
+const providerGoogle = new GoogleAuthProvider();
+const providerFacebook = new FacebookAuthProvider();
+const providerTwitter = new TwitterAuthProvider();
+const auth = getAuth();
 
 const SignIn = ({ signIn, setSignIn }) => {
-  // const handleSignInGithub = async () => {
-  //   try {
-  //     const result = await signInWithPopup(auth, providerGithub);
-  //     console.log(result);
+  const handleSignInGoogle = async () => {
+    try {
+      const result = await signInWithPopup(auth, providerGoogle);
+      console.log(result.providerId, result.user.uid, result.user.displayName);
 
-  //     const { email: username, email } = result.user;
-  //     // await this.githubSignIn({ username, email });
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
+      // await this.githubSignIn({ username, email });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const handleSignInFacebook = async () => {
+    try {
+      const result = await signInWithPopup(auth, providerFacebook);
+      console.log(result.providerId, result.user.uid, result.user.displayName);
+
+      // await this.githubSignIn({ username, email });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const handleSignInTwitter = async () => {
+    try {
+      const result = await signInWithPopup(auth, providerTwitter);
+      console.log(result.providerId, result.user.uid, result.user.displayName);
+
+      // await this.githubSignIn({ username, email });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div
@@ -29,8 +50,7 @@ const SignIn = ({ signIn, setSignIn }) => {
         signIn
           ? "fixed left-0 bottom-0 w-screen h-screen flex justify-center items-center bg-none z-[199] duration-300 ease-in"
           : "fixed left-0 bottom-[100vh] w-screen h-screen flex justify-center items-center bg-none z-[199] duration-300 ease-in"
-      }
-    >
+      }>
       <div
         className={
           signIn
@@ -40,22 +60,29 @@ const SignIn = ({ signIn, setSignIn }) => {
         onClick={() => setSignIn(false)}
       />
       <div className="flex justify-center items-center absolute inset-0">
-        <div className="w-96 max-w-[100vw] h-60 bg-white z-[200]">
-          <p className="justify-center flex items-center py-2">
-            Sign in to InvestMate
-          </p>
+        <div className="w-96 max-w-[100vw] h-auto bg-white z-[200]">
+          <div className="justify-center flex flex-col items-center py-2 h-32 bg-slate-200 bg-gradient-to-r from-gray-100 to-slate-300 m-2">
+            <p className="font-bold">InvestMate</p>
+            <p>Sign in to invest!</p>
+          </div>
           <div className="mx-2">
-            <button className="flex h-12 w-full items-center  px-4 py-2  text-white bg-blue-800 hover:bg-blue-600 mb-2">
+            <button
+              className="flex h-12 w-full items-center  px-4 py-2  text-white bg-red-500 hover:bg-red-600 mb-2"
+              onClick={handleSignInGoogle}>
+              <FaGoogle className="mr-2 text-xl" />
+              <p className="font-medium">Sign in with Google</p>
+            </button>
+            <button
+              className="flex h-12 w-full items-center  px-4 py-2  text-white bg-blue-800 hover:bg-blue-600 mb-2"
+              onClick={handleSignInFacebook}>
               <FaFacebook className="mr-2 text-xl" />
               <p className="font-medium">Sign in with Facebook</p>
             </button>
-            <button className="flex h-12 w-full items-center  px-4 py-2  text-white bg-blue-500 hover:bg-blue-400 mb-2">
+            <button
+              className="flex h-12 w-full items-center  px-4 py-2  text-white bg-blue-500 hover:bg-blue-400 mb-2"
+              onClick={handleSignInTwitter}>
               <FaTwitter className="mr-2 text-xl" />
               <p className="font-medium">Sign in with Twitter</p>
-            </button>
-            <button className="flex h-12 w-full items-center  px-4 py-2  text-white bg-red-500 hover:bg-red-600 mb-2">
-              <FaGoogle className="mr-2 text-xl" />
-              <p className="font-medium">Sign in with Google</p>
             </button>
           </div>
         </div>
