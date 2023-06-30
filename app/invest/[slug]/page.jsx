107524537b602f0e0f3736aaca883b42640c0410
yonatan-nyo <yonatan.njoto@gmail.com>
2023-06-30 13:38:38 +0700

    added detail

---
+++ app/invest/[slug]/page.jsx
@@ -0,0 +1,78 @@
+"use client";
+import { useEffect } from "react";
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import Image from "next/image";
+
+mapboxgl.accessToken = "pk.eyJ1IjoiaHVpZ2kiLCJhIjoiY2xnYjhxbzdhMXA4ZTNsbzd2Nm80OWsycSJ9.bIZhzPsqKFWtpMgJHDfM7Q";
+
+const DetailPage = () => {
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: "map", // container ID
+      style: "mapbox://styles/mapbox/streets-v12", // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+      interactive: false,
+    });
+
+    return () => {
+      map.remove();
+    };
+  }, []);
+
+  return (
+    <div className="flex flex-col justify-between min-h-screen text-center">
+      <div id="map" className="w-screen mt-20 h-[25vh] bg-[#ebebeb] absolute top-0 left-0" />
+
+      {/* Content */}
+      <div className="absolute top-0 left-0 mt-20 py-[15vh] h-auto flex flex-col w-screen mx-auto">
+        <section className="w-full max-w-[1450px] mx-auto flex justify-start overflow-x-hidden">
+          <div className="h-[20vh] px-4">
+            <Image
+              className="object-cover w-auto h-full"
+              width={400}
+              height={400}
+              src="https://upload.wikimedia.org/wikipedia/id/thumb/d/d3/Starbucks_Corporation_Logo_2011.svg/1200px-Starbucks_Corporation_Logo_2011.svg.png"
+              alt="IMAGE"
+            />
+          </div>
+          <div className="text-left flex flex-col justify-end">
+            <p className="font-bold text-xl bg-white/80 w-fit p-2 uppercase rounded-md">Category</p>
+            <p className="font-bold text-4xl mt-4">Business Name</p>
+            <div className="flex gap-4 mt-2">
+              <button className="bg-blue-500 hover:bg-blue-700 text-white rounded-md shadow-lg">INVEST</button>
+              <button className="bg-red-500 hover:bg-red-700 text-white rounded-md shadow-lg">PROSPEKTUS</button>
+            </div>
+          </div>
+        </section>
+        <section className="w-full max-w-[1450px] mx-auto flex justify-start mt-10 gap-4">
+          <div className="w-80 h-80 border-4 border-slate-300 flex-shrink-0">
+            <Image
+              className="object-cover w-80 h-80"
+              width={400}
+              height={400}
+              src="https://upload.wikimedia.org/wikipedia/id/thumb/d/d3/Starbucks_Corporation_Logo_2011.svg/1200px-Starbucks_Corporation_Logo_2011.svg.png"
+              alt="IMAGE"
+            />
+          </div>
+          <div className="flex-grow text-left ml-auto">
+            <p className="font-bold">Overview</p>
+            <p className="font-light">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu orci id massa pharetra tristique quis ut libero.
+              Sed risus sem, suscipit vel volutpat eget, lobortis nec enim. Ut feugiat dui ipsum, vel porttitor orci sagittis sed.
+              Integer et leo fermentum, venenatis nibh ac, semper massa. Vivamus rutrum imperdiet lobortis. Phasellus hendrerit
+              nulla non ante ullamcorper fermentum. Nulla in purus ut sem semper convallis. In molestie diam in vehicula pretium.
+              Sed lectus felis, mattis at vestibulum sed, tristique tincidunt risus. Sed non feugiat dolor. Maecenas mattis
+              viverra mauris vitae scelerisque. Quisque volutpat semper gravida. Morbi pharetra lacinia gravida. Fusce sit amet
+              viverra sapien. Phasellus rutrum felis et justo malesuada, ut sodales urna consectetur. Mauris ac eleifend diam, in
+              pellentesque enim.
+            </p>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default DetailPage;
