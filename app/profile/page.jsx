875f7a94e9563bc4ba1198797d1491f23c51f8a3
yonatan-nyo <yonatan.njoto@gmail.com>
2023-07-03 12:20:28 +0700

    edit profile page view

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -43,8 +43,8 @@ const Profile = () => {
   return (
     <>
       <SignIn setSignIn={setSignIn} signIn={signIn} />
-      <div className="flex flex-col pt-28 max-w-[1480px] h-screen mx-auto">
-        <div className="flex flex-wrap justify-center sm:justify-start px-6">
+      <div className="flex flex-row pt-28 max-w-[1480px] h-screen mx-auto pb-20">
+        <div className="flex flex-col w-[500px] justify-start px-6 h-full items-start bg-slate-100 mr-3 pt-4 rounded-xl">
           <div className="h-48 w-48 sm:w-auto rounded-lg overflow-hidden mb-4">
             <Image
               className="object-cover w-full h-full"
@@ -54,21 +54,34 @@ const Profile = () => {
               alt="IMAGE"
             />
           </div>
-          <div className="h-auto w-auto flex flex-col items-center sm:items-start px-8 justify-between py-3">
-            <div>
-              <h2 className="text-2xl font-bold">{profile?.username ?? "Account Name"}</h2>
+          <div className="flex flex-wrap justify-center sm:justify-start px-4">
+            <div className="h-auto w-auto flex flex-col items-center sm:items-start justify-between py-3">
+              <div className="mb-2">
+                <h2 className="text-2xl font-bold">{profile?.username ?? "Account Name"}</h2>
+              </div>
+              <div className="flex justify-center sm:justify-start">
+                <button className="bg-red-500 text-white px-4 py-2 rounded-full mt-2" onClick={logout}>
+                  <p className="font-bold">LOGOUT</p>
+                </button>
+              </div>
             </div>
-            <button className="bg-red-500 text-white px-4 py-2 rounded-full mt-2" onClick={logout}>
-              <p className="font-bold">LOGOUT</p>
-            </button>
           </div>
         </div>
 
-        <div className="flex flex-wrap justify-start space-y-4 px-6 mt-4 w-full">
-          <div className=" flex-auto flex-wrap w-full sm:w-1/2 h-40 bg-gray-200 rounded mb-2 sm:mr-2 sm:mb-0 mr-2">
-            <p className="font-bold text-xl ml-2">Business</p>
+        <div className="flex flex-wrap justify-start space-y-4 px-6 mt-4 w-full mb-10 border-l-2 border-slate-400">
+          <div className="flex-grow h-auto w-full sm:w-1/2 rounded mb-2 sm:mr-2 sm:mb-0 mr-2">
+            <div className="flex items-center mt-2">
+              <p className="font-bold text-xl ml-2 mr-2">Business</p>
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white rounded-md shadow-lg flex py-1 px-2"
+                onClick={() => router.push("/invest/add")}>
+                <p className="flex items-center gap-2">
+                  <span className="text-2xl p-0">+</span>Add
+                </p>
+              </button>
+            </div>
           </div>
-          <div className="flex-auto w-full flex-wrap sm:w-1/2 h-40 bg-gray-200 rounded mb-2 sm:mr-2 sm:mb-0 mr-2">
+          <div className="flex-auto w-full sm:w-1/2 h-auto rounded mb-2 sm:mr-2 sm:mb-0 mr-2 border-t-2 pt-2 border-slate-400">
             <p className="font-bold text-xl ml-2">Investments</p>
           </div>
         </div>
