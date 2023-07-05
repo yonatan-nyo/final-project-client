2b643af05a20071f48f157b6599226885cdcc2de
yonatan-nyo <yonatan.njoto@gmail.com>
2023-07-05 20:52:20 +0700

    fixed background for image

--- app/invest/[slug]/page.jsx
+++ app/invest/[slug]/page.jsx
@@ -17,8 +17,7 @@ const stripePromise = loadStripe(
   "pk_test_51NPcQ6ISk7K0qdKAKVPttTgpEXm5kd34yTtUurAg1YQxeAVqRFKwMg5SAqcWdtoFWDHxJpuAG9xzztvjiYWJNEdc00NW8JDNeH"
 );
 
-mapboxgl.accessToken =
-  "pk.eyJ1IjoiaHVpZ2kiLCJhIjoiY2xnYjhxbzdhMXA4ZTNsbzd2Nm80OWsycSJ9.bIZhzPsqKFWtpMgJHDfM7Q";
+mapboxgl.accessToken = "pk.eyJ1IjoiaHVpZ2kiLCJhIjoiY2xnYjhxbzdhMXA4ZTNsbzd2Nm80OWsycSJ9.bIZhzPsqKFWtpMgJHDfM7Q";
 
 const DetailPage = ({ params }) => {
   const router = useRouter();
@@ -152,17 +151,14 @@ const DetailPage = ({ params }) => {
       <ToastContainer />
 
       <div className="flex flex-col justify-between min-h-screen text-center">
-        <div
-          id="map"
-          className="w-screen mt-20 h-[25vh] bg-[#ebebeb] sticky top-0 left-0 z-0"
-        />
+        <div id="map" className="w-screen mt-20 h-[25vh] bg-[#ebebeb] sticky top-0 left-0 z-0" />
 
         {/* Content */}
         <div className="absolute top-0 left-0 mt-20 py-[16vh] h-auto flex flex-col w-screen mx-auto">
           <section className="w-full max-w-[1450px] mx-auto flex justify-start overflow-x-hidden">
             <div className="h-[20vh] px-4">
               <Image
-                className="object-cover w-auto h-full rounded-lg border-4 border-slate-200"
+                className="object-cover w-auto h-full rounded-lg border-4 border-slate-200 bg-white"
                 width={400}
                 height={400}
                 src={
@@ -184,10 +180,7 @@ const DetailPage = ({ params }) => {
               <p className="font-bold text-4xl mt-4">{data?.name}</p>
               <div className="flex gap-4 mt-2">
                 {showInvest && (
-                  <a
-                    className="bg-blue-500 hover:bg-blue-700 text-white rounded-md shadow-lg py-1 px-2"
-                    onClick={payment}
-                  >
+                  <a className="bg-blue-500 hover:bg-blue-700 text-white rounded-md shadow-lg py-1 px-2" onClick={payment}>
                     PRE-ORDER Rp 100.000,00
                   </a>
                 )}
@@ -195,8 +188,7 @@ const DetailPage = ({ params }) => {
                   href={data?.pdfUrl}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="border-2 border-blue-600 bg-white hover:brightness-90 rounded-md shadow-lg w-fit py-1 px-2 flex justify-center items-center"
-                >
+                  className="border-2 border-blue-600 bg-white hover:brightness-90 rounded-md shadow-lg w-fit py-1 px-2 flex justify-center items-center">
                   <p className="text-blue-500">PROSPEKTUS</p>
                 </a>
               </div>
@@ -226,11 +218,7 @@ const DetailPage = ({ params }) => {
       {options?.clientSecret && showCheckout && (
         <div className="Stripe fixed top-0 flex w-screen h-screen justify-center items-center">
           <Elements options={options} stripe={stripePromise}>
-            <CheckoutForm
-              slug={params.slug}
-              detail={dataResPayment}
-              setShowCheckout={setShowCheckout}
-            />
+            <CheckoutForm slug={params.slug} detail={dataResPayment} setShowCheckout={setShowCheckout} />
           </Elements>
         </div>
       )}
