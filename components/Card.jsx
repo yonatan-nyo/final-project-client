ff2068f53e818c3ad2472d68fa91a220b8070990
yonatan-nyo <yonatan.njoto@gmail.com>
2023-07-05 21:17:06 +0700

    fixed styling for card

--- components/Card.jsx
+++ components/Card.jsx
@@ -13,8 +13,8 @@ const Card = ({ data }) => {
     <div className="w-[96%] md:w-[47%] lg:w-[32%] h-56 md:h-52 bg-slate-100 mx-auto relative rounded-xl shadow-lg">
       <div className="w-full absolute h-12 bg-blue-300 bg-gradient-to-r from-sky-300 to-blue-500 rounded-t-lg" />
       <div className="flex absolute p-8 gap-8 flex-row text-right w-full h-full">
-        <div className="w-40 h-[190px] flex-col justify-between">
-          <div>
+        <div className="w-32 h-[190px] flex flex-col">
+          <div className="w-32">
             <Image
               src={
                 data?.brandUrl ??
@@ -26,7 +26,7 @@ const Card = ({ data }) => {
               className="bg-white rounded-lg border-2 border-slate-200 w-full h-28 object-contain"
             />
           </div>
-          <div className="mt-4 leading-relaxed font-semibold w-full bg-white border-2 border-slate-400 h-4 overflow-hidden rounded-full">
+          <div className="w-32 mt-4 leading-relaxed font-semibold bg-white border-2 border-slate-400 h-4 overflow-hidden rounded-full">
             <div
               className="h-4 w-[5%] bg-green-500"
               style={{
@@ -34,10 +34,10 @@ const Card = ({ data }) => {
               }}
             />
           </div>
-          <p className="text-center">{data?.fundReceived?.length} / 40</p>
+          <p className="text-center w-full">{data?.fundReceived?.length} / 40</p>
         </div>
-        <div className="flex flex-col pt-6 w-full">
-          <p className="text-left font-bold text-2xl">{data?.name}</p>
+        <div className="flex flex-col pt-6 item-start flex-grow">
+          <p className="text-left font-bold text-2xl w-full">{data?.name}</p>
           <p className="text-left">
             {Math.ceil(+data?.fundNeeded / 40).toLocaleString("id-ID", {
               style: "currency",
@@ -47,8 +47,7 @@ const Card = ({ data }) => {
           <div className="flex-grow flex justify-end items-end">
             <a
               className="bg-blue-500 hover:bg-blue-700 w-full text-white font-bold py-2 px-6 rounded-full"
-              onClick={() => handleClick(data?.slug)}
-            >
+              onClick={() => handleClick(data?.slug)}>
               <p>SEE DETAIL</p>
             </a>
           </div>
